e189906162dee1ca86d6dcc69833ee09fb5aa6a7
faea726 <fantasy.eagle96@gmail.com>
2022-12-20 08:42:35 +0700

    11. Container with Most Water

--- container-with-most-water-11/src/main.rs
+++ container-with-most-water-11/src/main.rs
@@ -1,7 +1,21 @@
 struct Solution {}
 impl Solution {
     pub fn max_area(height: Vec<i32>) -> i32 {
-        height.len() as i32
+        let mut max_area = 0;
+        let mut left = 0;
+        let mut right = height.len() - 1;
+        while left < right {
+            let area = (right - left) as i32
+                * if height[left] < height[right] {
+                    left += 1;
+                    height[left - 1]
+                } else {
+                    right -= 1;
+                    height[right + 1]
+                };
+            max_area = if area > max_area { area } else { max_area };
+        }
+        max_area
     }
 }
 fn main() {
