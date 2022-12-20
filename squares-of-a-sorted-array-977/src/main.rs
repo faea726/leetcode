4384010847bf53e0df75f44a04478f9a55c46238
faea726 <fantasy.eagle96@gmail.com>
2022-12-20 16:29:06 +0700

    udpate solution

--- squares-of-a-sorted-array-977/src/main.rs
+++ squares-of-a-sorted-array-977/src/main.rs
@@ -1,25 +1,29 @@
 struct Solution {}
 impl Solution {
-    fn sorted_squares(nums: Vec<i32>) -> Vec<i32> {
-        let mut result = Vec::new();
+    pub fn sorted_squares(a: Vec<i32>) -> Vec<i32> {
+        if a.len() == 1 {
+            return vec![a[0] * a[0]];
+        }
+        let mut res = vec![0; a.len()];
         let mut i = 0;
-        let mut j = nums.len() - 1;
+        let mut j = a.len() - 1;
+        let mut k = a.len() - 1;
         while i <= j {
-            if nums[i].abs() > nums[j].abs() {
-                result.push(nums[i] * nums[i]);
+            if a[i].pow(2) >= a[j].pow(2) {
+                res[k] = a[i] * a[i];
                 i += 1;
             } else {
-                result.push(nums[j] * nums[j]);
+                res[k] = a[j] * a[j];
                 j -= 1;
             }
+            k -= 1;
         }
-        result.reverse();
-        result
+        res
     }
 }
 
 fn main() {
-    let input = vec![-5, 4, 3, -9, 4, 8, 6];
+    let input = vec![-7, -3, 2, 3, 11];
     let result = Solution::sorted_squares(input);
-    println!("{:?}", result)
+    println!("{:?}", result);
 }
